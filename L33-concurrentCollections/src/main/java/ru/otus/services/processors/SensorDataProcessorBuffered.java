ae538b26f2f286c7b83c4149ae1ba55ef4363457
pavel-krasyukov <rusmoroz@yandex.ru>
2025-01-09 10:38:25 +0300

    L33-concurrentCollections (д/з Красюков П.А.)

---
+++ L33-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -0,0 +1,48 @@
+package ru.otus.services.processors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.api.SensorDataProcessor;
+import ru.otus.api.model.SensorData;
+import ru.otus.lib.SensorDataBufferedWriter;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+// Этот класс нужно реализовать
+@SuppressWarnings({"java:S1068", "java:S125"})
+public class SensorDataProcessorBuffered implements SensorDataProcessor {
+    private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
+
+    private final int bufferSize;
+    private final SensorDataBufferedWriter writer;
+
+    private List<SensorData> bufferedData = new CopyOnWriteArrayList();
+
+    public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
+        this.bufferSize = bufferSize;
+        this.writer = writer;
+    }
+
+    @Override
+    public void process(SensorData data) {
+        bufferedData.add(data);
+        if (bufferedData.size() >= bufferSize) {
+            flush();
+        }
+    }
+
+    public void flush() {
+        try {
+            writer.writeBufferedData(bufferedData);
+            //todo почистить лист!
+        } catch (Exception e) {
+            log.error("Ошибка в процессе записи буфера", e);
+        }
+    }
+
+    @Override
+    public void onProcessingEnd() {
+        flush();
+    }
+}
