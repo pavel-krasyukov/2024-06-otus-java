66c02b4075b7b9768ad936a492044428509116d7
pavel-krasyukov <rusmoroz@yandex.ru>
2025-01-09 18:41:43 +0300

    L31-executors (д/з Красюков П.А.) исправления

--- L31-executors/src/main/java/ru/otus/Main.java
+++ L31-executors/src/main/java/ru/otus/Main.java
@@ -3,72 +3,57 @@
  */
 package ru.otus;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
-
     private static final Lock lock = new ReentrantLock();
-    private static volatile boolean isThreadOneTurn = true; // true, если первый поток должен выполнять свою задачу
-
-    private static final String THREAD1_NAME = "Поток 1";
-    private static final String THREAD2_NAME = "Поток 2";
+    private static final Condition condition = lock.newCondition();
+    private static boolean isThreadOneTurn = true;
 
     public static void main(String[] args) {
-	Thread thread1 = new Thread(new NumberPrinter(THREAD1_NAME));
-	Thread thread2 = new Thread(new NumberPrinter(THREAD2_NAME));
+	Thread thread1 = new Thread(new NumberPrinter(1));
+	Thread thread2 = new Thread(new NumberPrinter(2));
 
 	thread1.start();
 	thread2.start();
     }
 
     static class NumberPrinter implements Runnable {
-	private String threadName;
+	private final int threadId;
 
-	public NumberPrinter(String threadName) {
-	    this.threadName = threadName;
+	public NumberPrinter(int threadId) {
+	    this.threadId = threadId;
 	}
 
 	@Override
 	public void run() {
-	    printNumbers(1, 10);
+	    while (true) {
+		// Сначала печатаем от 1 до 10
+		for (int i = 1; i <= 10; i++) {
+		    printNumber(i);
+		}
+		// Затем печатаем от 9 до 1
+		for (int i = 9; i >= 1; i--) {
+		    printNumber(i);
+		}
+	    }
 	}
 
-	private void printNumbers(int start, int end) {
-	    while (true) {
-		lock.lock(); // блокируем
-		try {
-		    //Поток1
-		    if (THREAD1_NAME.equals(threadName) && isThreadOneTurn) {
-			System.out.print(THREAD1_NAME+": ");
-			for (int i = start; i <= end; i++) {
-			    System.out.print(i + " ");
-			}
-			for (int i = end - 1; i >= start; i--) {
-			    System.out.print(i + " ");
-			}
-			System.out.println();
-			isThreadOneTurn = false; // Меняем флаг
-		    // Поток2
-		    } else if (THREAD2_NAME.equals(threadName) && !isThreadOneTurn) {
-			System.out.print(THREAD2_NAME+": ");
-			for (int i = start; i <= end; i++) {
-			    System.out.print(i + " ");
-			}
-			for (int i = end - 1; i >= start; i--) {
-			    System.out.print(i + " ");
-			}
-			System.out.println();
-			isThreadOneTurn = true;
-		    }
-		} finally {
-		    lock.unlock(); // разблокируем
+	private void printNumber(int number) {
+	    lock.lock();
+	    try {
+		while ((threadId == 1 && !isThreadOneTurn) || (threadId == 2 && isThreadOneTurn)) {
+		    condition.await();
 		}
-
-		try {
-		    Thread.sleep(100); // задержка, чтобы было легче читать вывод
-		} catch (InterruptedException e) {
-		    Thread.currentThread().interrupt();
-		}
+		System.out.println("Поток " + threadId + ": " + number);
+		isThreadOneTurn = !isThreadOneTurn; // Меняем очередь
+		condition.signalAll(); // Пробуждаем другой поток
+	    } catch (InterruptedException e) {
+		Thread.currentThread().interrupt();
+	    } finally {
+		lock.unlock();
 	    }
 	}
     }
