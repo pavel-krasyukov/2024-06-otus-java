4a6625f952215b2fab99ae68ad1bb40d4429eb83
pavel-krasyukov <rusmoroz@yandex.ru>
2025-01-14 21:14:41 +0300

    L38-webflux-chat (д/з Красюков П.А.)

---
+++ L38-webflux-chat/client-service/build.gradle.kts
@@ -0,0 +1,12 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation ("com.google.code.findbugs:jsr305:3.0.2")
+
+    implementation("org.webjars:webjars-locator-core")
+    implementation("org.webjars:sockjs-client:1.5.1")
+    implementation("org.webjars:stomp-websocket:2.3.4")
+    implementation("org.webjars:bootstrap:5.2.3")
+}
+
+
